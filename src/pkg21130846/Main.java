fb2e548a4f6e82679001132b4796138950ca0695
AL3x3x <alu.21130846@correo.itlalaguna.edu.mx>
2025-03-04 14:35:11 -0600

    Creando proyecto

---
+++ src/pkg21130846/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg21130846;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vale
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    int[][] matrix = new int[9][9];
+    ArrayList<Integer> unidimensional  = new ArrayList();
+    ArrayList<ArrayList> bidimensional  = new ArrayList();
+    
+    
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
